e26451d11786094d54f53735bc1dd01bfbcd6fee
yoloyachtclub <80938004+yoloyachtclub@users.noreply.github.com>
2021-05-03 14:56:28 -0700

    Update buildLinux.sh

--- buildLinux.sh
+++ buildLinux.sh
@@ -1,6 +1,32 @@
-make clean
-
+sudo apt install -y \
+  build-essential \
+  libtool \
+  autotools-dev \
+  automake \
+  pkg-config \
+  libssl-dev \
+  libevent-dev \
+  bsdmainutils \
+  libminiupnpc-dev \
+  libzmq3-dev \
+  libqt5gui5 \
+  libqt5core5a \
+  libqt5dbus5 \
+  qttools5-dev \
+  qttools5-dev-tools \
+  libprotobuf-dev \
+  protobuf-compiler \
+  libqrencode-dev \
+  libdb++-dev \
+  g++-mingw-w64-x86-64 \
+  nsis \
+  libboost-all-dev\
+  
 ./autogen.sh
-./configure --with-incompatible-bdb --with-gui=no --disable-tests --disable-man
-make -j 8
-make install
+./configure --disable-man --disable-tests --disable-bench --disable-gui-tests --with-incompatible-bdb
+make -j
+sudo make install
+
+# binaries are installed to /usr/local/bin
+# the GUI wallet is /usr/local/bin/yyccoin-qt
+
